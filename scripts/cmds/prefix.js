91b2245e425ce5b937001c9ece29300c15539d95
Ronaldo-7-M-10 <failedagain055@gmail.com>
2024-11-11 09:04:31 +0600

    Update prefix.js

--- scripts/cmds/prefix.js
+++ scripts/cmds/prefix.js
@@ -1,79 +1,29 @@
-const fs = require("fs-extra");
-const { utils } = global;
-const UpolUrl = ["https://i.imgur.com/NT6lmm3.gif"];
-const UpolImage = UpolUrl[Math.floor(Math.random() * UpolUrl.length)];
-
 module.exports = {
-  config: {
-    name: "prefix",
-    version: "1.3",
-    author: "NTKhang | UPoL🐔",
-    role: 0,
-  },
+ config: {
+	 name: "prefix",
+	 version: "1.0",
+	 author: "Tokodori_Frtiz",//remodified by cliff
+	 countDown: 5,
+	 role: 0,
+	 shortDescription: "no prefix",
+	 longDescription: "no prefix",
+	 category: "auto 🪐",
+ },
 
-  langs: {
-    en: {
-      reset: "Your prefix has been reset to default: %1",
-      onlyAdmin: "Only admin can change prefix of system bot",
-      confirmGlobal: "Please react to this message to confirm change prefix of system bot",
-      confirmThisThread: "Please react to this message to confirm change prefix in your box chat",
-      successGlobal: "Changed prefix of system bot to: %1",
-      successThisThread: "Changed prefix in your box chat to: %1",
-      myPrefix: " 🗡️ My Bankai is: %2"
-    }
-  },
-
-  onStart: async function ({ message, role, args, commandName, event, threadsData, getLang }) {
-    if (!args[0])
-      return message.SyntaxError();
-
-    if (args[0] == 'reset') {
-      await threadsData.set(event.threadID, null, "data.prefix");
-      return message.reply(getLang("reset", global.GoatBot.config.prefix));
-    }
-
-    const newPrefix = args[0];
-    const formSet = {
-      commandName,
-      author: event.senderID,
-      newPrefix
-    };
-
-    if (args[1] === "-g")
-      if (role < 2)
-        return message.reply(getLang("onlyAdmin"));
-      else
-        formSet.setGlobal = true;
-    else
-      formSet.setGlobal = false;
-
-    return message.reply(args[1] === "-g" ? getLang("confirmGlobal") : getLang("confirmThisThread"), (err, info) => {
-      formSet.messageID = info.messageID;
-      global.GoatBot.onReaction.set(info.messageID, formSet);
-    });
-  },
-
-  onReaction: async function ({ message, threadsData, event, Reaction, getLang }) {
-    const { author, newPrefix, setGlobal } = Reaction;
-    if (event.userID !== author)
-      return;
-    if (setGlobal) {
-      global.GoatBot.config.prefix = newPrefix;
-      fs.writeFileSync(global.client.dirConfig, JSON.stringify(global.GoatBot.config, null, 2));
-      return message.reply(getLang("successGlobal", newPrefix));
-    }
-    else {
-      await threadsData.set(event.threadID, newPrefix, "data.prefix");
-      return message.reply(getLang("successThisThread", newPrefix));
-    }
-  },
-
-  onChat: async function ({ event, message, getLang }) {
-    if (event.body && event.body.toLowerCase() === "prefix") {
-        const attachment = await utils.getStreamFromURL(UpolImage);
-      return () => {
-        return message.reply({ body: getLang("myPrefix", global.GoatBot.config.prefix, utils.getPrefix(event.threadID)), attachment: attachment });
-      };
-     }
-  }
-};
+ onStart: async function(){}, 
+ onChat: async function({ event, message, getLang }) {
+ if (event.body && event.body.toLowerCase() === "prefix") {
+ return message.reply({
+ body: `
+Yo, endy prefix is [ 𓆩 / 𓆪 ]\n
+Check here please:
+➥ %help [number of page] -> see commands
+🎶This bot created by Miles Morales..
+Acceptance admin is Miles 🕷️
+➥ %callad [message] -> report any problem encountered
+`,
+ attachment: await global.utils.getStreamFromURL("https://i.ibb.co/gFryKwf/image.jpg")
+ });
+ }
+ }
+}
