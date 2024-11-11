f8af277754a86a37ce04f55dba90fde38af18314
Ronaldo-7-M-10 <failedagain055@gmail.com>
2024-11-11 14:49:10 +0600

    Update prefix.js

--- scripts/cmds/prefix.js
+++ scripts/cmds/prefix.js
@@ -1,28 +1,22 @@
 module.exports = {
  config: {
-	 name: "prefix",
-	 version: "1.0",
-	 author: "Tokodori_Frtiz",//remodified by cliff
-	 countDown: 5,
-	 role: 0,
-	 shortDescription: "no prefix",
-	 longDescription: "no prefix",
-	 category: "auto 🪐",
+   name: "prefix",
+   version: "1.0",
+   author: "MR.AYAN",
+   countDown: 5,
+   role: 0,
+   shortDescription: "no prefix",
+   longDescription: "no prefix",
+   category: "auto 🪐",
  },
 
  onStart: async function(){}, 
  onChat: async function({ event, message, getLang }) {
  if (event.body && event.body.toLowerCase() === "prefix") {
  return message.reply({
- body: `
-Yo, endy prefix is [ 𓆩 / 𓆪 ]\n
-Check here please:
-➥ %help [number of page] -> see commands
-🎶This bot created by Miles Morales..
-Acceptance admin is Miles 🕷️
-➥ %callad [message] -> report any problem encountered
+ body: `Hello bro\n\nMy prefix is [ / ]\n\n______________________________\n\n𝗛𝗢𝗪 𝗧𝗢 𝗨𝗦𝗘?\nexample please type: /help - to view all cmds \n𝗕𝗢𝗧 𝗢𝗪𝗡𝗘𝗥: Miles ツ\n______________________________
 `,
- attachment: await global.utils.getStreamFromURL("https://i.ibb.co/gFryKwf/image.jpg")
+  attachment: await global.utils.getStreamFromURL("https://i.ibb.co/6szNvpK/image.gif")
  });
  }
  }
